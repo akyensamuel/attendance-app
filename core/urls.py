95b177b6f82fc59e8590d752a129b534177935a2
Wodoame <bmwodoame@gmail.com>
2023-12-02 10:19:53 +0000

    added a login page

--- core/urls.py
+++ core/urls.py
@@ -3,5 +3,7 @@ from . import views
 urlpatterns = [
     path('', views.test, name='index'),
     path('admin-page/', views.admin, name='admin-page'),
+    path('admin-login/', views.adminLogin, name='admin-login'),
+    path('admin-logout/', views.adminLogout, name='admin-logout'),
     path('test/', views.index, name='test'),
 ]
--- core/views.py
+++ core/views.py
@@ -2,10 +2,13 @@ from django.shortcuts import render, redirect
 from django.forms.models import modelform_factory
 from .models import Absentee
 from datetime import datetime, timedelta 
-
+from django.contrib.auth import login, authenticate, logout
+from django.contrib import messages
+from django.contrib.auth.decorators import login_required
 def index(request):
     return render(request, 'index.html', {})
 
+@login_required(login_url='admin-login')
 def admin(request):
     AbsenteeForm = modelform_factory(model=Absentee, fields='__all__')
     if request.method == 'GET':
@@ -24,3 +27,24 @@ def test(request):
     absenteesYesterday = Absentee.objects.filter(date=dateYesterday)
     days = [{'label':'Yesterday', 'list': absenteesYesterday}, {'label':'Today', 'list': absenteesToday}]
     return render(request, 'table.html', {'days': days})
+
+def adminLogin(request):
+    if request.method == 'GET':
+        return render(request, 'login.html')
+    else: 
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        user = authenticate(request, username=username, password=password)
+        if user and user.is_superuser:
+            login(request, user)
+            messages.success(request, 'Logged in successfully')
+            return redirect('admin-page')
+        else: 
+            messages.error(request, 'Access Denied')
+            return render(request, 'login.html', {})
+        
+def adminLogout(request):
+    logout(request)
+    return redirect('admin-login')
+    
+# how to check if a user is a super user in django ?
